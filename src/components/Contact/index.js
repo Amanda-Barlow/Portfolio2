45af531fd981bcc8d05c7dfc3f429ed9e05843d6
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-06-29 19:56:19 -0800

    added resume link

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs from  '@emailjs/browser';
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
-import './index.scss'
+import './index.scss';
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -18,18 +18,17 @@ const Contact = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs
-      .sendForm('gmail', 'service_dno42yk', refForm.current, '5JM5_hQii-VjdpfIb')
-      .then(
-        () => {
-          alert('Message successfully sent!');
-          window.location.reload(false);
-        },
-        () => {
-          alert('Failed to send the message, please try again');
-        }
-      );
+    
+    emailjs.sendForm('gmail', 'template_cekkzso', e.target, '5JM5_hQii-VjdpfIb')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('Message successfully sent!');
+        window.location.reload(false);
+      })
+      .catch((error) => {
+        console.error('FAILED...', error);
+        alert('Failed to send the message, please try again');
+      });
   };
 
   return (
@@ -44,7 +43,7 @@ const Contact = () => {
           <h3>I am interested in finding the right Software Developer role.</h3>
 
           <div className="contact-form">
-            <form ref = { refForm } onSubmit= { sendEmail } >
+            <form ref={refForm} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
                   <input type="text" name="name" placeholder="Name" required />
@@ -67,7 +66,6 @@ const Contact = () => {
         </div>
         <Loader type="pacman" />
       </div>
-      
     </>
   );
 };
