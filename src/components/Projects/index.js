e781d9e086d7fcf9caed57ef3657806655d1abc4
Amanda-Barlow <119551269+Amanda-Barlow@users.noreply.github.com>
2023-07-01 12:39:18 -0800

    ready to deplay

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './index.scss';
-import React from 'react';
 
 
-const Projects = () => {
-    <div className = 'recentProj'>
+
+
+const Projects = (props) => {
+   const { img, disc } = props.items
+   return (
+   <div className = 'recentProj'>
+        
         <h1>My Treatment, My Plan</h1>
         <h2>Project desc</h2>
         <Link to = "https://github.com/Amanda-Barlow/MyPlanBackend"></Link>
@@ -13,8 +18,8 @@ const Projects = () => {
         <img src="/assets/moose.jpeg" alt="Moose" style={{ maxWidth: '50%', maxHeight: '50%' }}/>
         <p className="legend">Moose in Alaska</p>
       </div>
-
-    </div>
+    </div>   
+   )
 };
 
 export default Projects;
